d80c14ac2ff3e4b4d6d2526ac3854521531f081b
sparban <estebanarbe55@gmail.com>
2022-02-05 18:23:29 -0500

    Creacion del turno de la computadora y del boton detener

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -4,6 +4,20 @@
 let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const cartasEspeciales = ['J', 'Q', 'K', 'A'];
+let puntosJugador = 0;
+let puntosComputadora = 0;
+
+// Referencias de html
+
+const btnPedir = document.querySelector('#btnPedir');
+const btnbtnDetener = document.querySelector('#btnDetener');
+const btnNuevo = document.querySelector('#btnNuevo');
+// Incluido el resultado del jugador y de la computadora.
+const resultadoJugador = document.querySelectorAll('small'); 
+const divCartasJugador = document.querySelector('#jugador-cartas');
+const divCartasComputadora = document.querySelector('#computadora-cartas');
+
+
 const crearDeck = () => {
     for(let i = 2; i <=10; i++){
         for (let tipo of tipos){
@@ -70,6 +84,70 @@ const valorCarta2 = (carta) => {
                     : valor*1;
 }
 
-const valor = valorCarta2(pedirCarta());
 
-console.log(valor);
+
+// Eventos
+
+btnPedir.addEventListener('click', ()=>{
+
+    const carta = pedirCarta();
+    const valor = valorCarta2(carta);
+    puntosJugador = puntosJugador + valor;
+    resultadoJugador[0].innerText = puntosJugador;
+
+    // Adicionar imagen de la carta al DOM
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${carta}.png`;
+    imgCarta.classList.add('carta');
+    divCartasJugador.append(imgCarta);
+
+    //<img class = "carta" src="assets/cartas/2C.png" alt=""></img>
+     
+    // Evaluar el juego para determinar el ganador
+    
+    const mensaje = (puntosJugador > 21)  ? (btnPedir.disabled = true, 
+                                            btnDetener.disabled = true, 
+                                            console.log('Perdiste'),
+                                            turnoComputadora(puntosJugador)):
+                    (puntosJugador === 21)? (btnPedir.disabled = true, 
+                                            btnDetener.disabled = true, 
+                                            console.log('Great, you could win'),
+                                            turnoComputadora(puntosJugador)):
+                                            console.log('continue with more cards'); 
+                                               
+});
+
+    // Logica del boton de detener.
+
+    btnbtnDetener.addEventListener('click', ()=>{
+        turnoComputadora(puntosJugador);
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+    });
+
+    // Turno computadora
+const turnoComputadora = (puntosMinimos) => {
+
+    do {
+    const carta = pedirCarta();
+    const valor = valorCarta2(carta);
+    puntosComputadora = puntosComputadora + valor;
+    // resultado de la computadora
+    resultadoJugador[1].innerText = puntosComputadora;
+
+    // Adicionar imagen de la carta al DOM
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${carta}.png`;
+    imgCarta.classList.add('carta');
+    // Agregar carta al div de html
+    divCartasComputadora.append(imgCarta);
+
+    if (puntosMinimos > 21){
+        break;
+    }
+
+    } while((puntosComputadora < puntosMinimos) && (puntosMinimos < 21));
+}     
+
+// Todo:borrar
+
